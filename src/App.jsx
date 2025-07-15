f2557b4af03f32c0f557829d132b6dd7ed191b30
vineesh23 <vinishb24@gmail.com>
2025-07-15 22:41:47 +0530

    Initial commit of zod_form project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SimpleForm from './components/simple-form'
+function App() {
+  
+
+  return (
+    <>
+     <SimpleForm />
+        
+    </>
+  )
+}
+
+export default App
