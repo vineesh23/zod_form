1b3773157ed2b66a083e87834d0ae00e90111d38
vineesh23 <vinishb24@gmail.com>
2025-07-16 12:36:42 +0530

    created zod form

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SimpleForm from './components/simple-form'
+import HookForm from './components/hooks-form'
+import ZodForm from './components/zod-form'
 function App() {
   
 
   return (
     <>
-     <SimpleForm />
+     <ZodForm />
         
     </>
   )
